3e348586f486b4fb43d629494cb465fb75168033
Harsh Singh <harsh0singh25@gmail.com>
2026-07-17 23:39:55 +0530

    Complete Backend

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,15 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://electro-fix-nu.vercel.app/"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 
 app.use(express.json());
 app.get("/", (req, res) => {
--- Frontend/js/booking.js
+++ Frontend/js/booking.js
@@ -67,7 +67,7 @@ form.addEventListener("submit", async function (e) {
 
     try {
 
-        const response = await fetch("http://localhost:5000/api/booking", {
+        const response = await fetch("https://electrofix-backend-dv1u.onrender.com/api/booking", {
 
             method: "POST",
 
