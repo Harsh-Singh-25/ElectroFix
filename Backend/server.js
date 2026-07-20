2c0b95df56dbaaf975893f3b855eaafe1b484e30
Harsh Singh <harsh0singh25@gmail.com>
2026-07-20 11:08:13 +0530

    SMTP

--- Backend/server.js
+++ Backend/server.js
@@ -1,49 +1,48 @@
-const dns = require("dns");
+    const dns = require("dns");
 
-dns.setDefaultResultOrder("ipv4first");
+    dns.setDefaultResultOrder("ipv4first");
 
-const dotenv = require("dotenv");
-dotenv.config();
-const result = dotenv.config();
+    const dotenv = require("dotenv");
+    dotenv.config();
 
-const express = require("express");
+    const express = require("express");
 
-const cors = require("cors");
+    const cors = require("cors");
 
-const connectDB = require("./config/db");
-connectDB();
+    const connectDB = require("./config/db");
+    connectDB();
 
-const PORT = process.env.PORT || 5000;
+    const PORT = process.env.PORT || 5000;
 
-const allowedOrigins = [
-    "http://127.0.0.1:5500",
-    "http://localhost:5500",
-    "https://electro-fix-nu.vercel.app",
-    "https://electro-fix-tawny.vercel.app"
-];
-
+    const allowedOrigins = [
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "https://electro-fix-nu.vercel.app",
+        "https://electro-fix-tawny.vercel.app"
+    ];
 
 
-const app = express();
 
-app.use(cors({
-    origin: function(origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true
-}));
+    const app = express();
 
-app.use(express.json());
-app.get("/", (req, res) => {
-    res.send("Server is working");
-});
+    app.use(cors({
+        origin: function(origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        credentials: true
+    }));
 
-app.use("/api/booking",require("./routes/bookingRoutes"));
+    app.use(express.json());
+    app.get("/", (req, res) => {
+        res.send("Server is working");
+    });
 
-app.listen(PORT, () => {
-    console.log(`Server Running on Port ${PORT}`);
-});;
+    app.use("/api/booking",require("./routes/bookingRoutes"));
+
+    app.listen(PORT, () => {
+        console.log(`Server Running on Port ${PORT}`);
+    });;
